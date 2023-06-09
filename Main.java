dfe26a41128d65336e09dfed173951f57cf917fd
farzad <farzad.ghlm8@gmail.com>
2023-06-09 13:18:15 -0600

    class binary search

--- Main.java
+++ Main.java
@@ -8,7 +8,29 @@ public class Main {
             }
         }
         return -1;
-    } 
+    }
+
+    public static int binarySearch(int[] numbers, int target) {
+        int low = 0;
+        int high = numbers.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (numbers[mid] == target) {
+                return mid;
+            } else if (numbers[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    
+
+    
 
         
 }
