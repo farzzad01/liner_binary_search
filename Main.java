1babb8b17ae2adf186a1df40aa3ab920c535ea7e
farzad <farzad.ghlm8@gmail.com>
2023-06-09 13:17:51 -0600

    main class

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int linearSearch(int[] numbers, int target) {
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    } 
+
+        
+}
